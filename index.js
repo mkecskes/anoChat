f5e9bdf359a7ad7cb937cd70a7a8a86c707ec60f
mkecskes <milan.kecskes@posteo.hu>
2016-07-30 13:33:07 +0200

    Quick css background fix for Android

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ io.on("connection", function(socket) {
     });
 });
 
-http.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 8080);
 
 function seekPartner(id, gender, age, seekGender, ageFrom, ageTo) {
     for (var partnerId in socketMap) {
